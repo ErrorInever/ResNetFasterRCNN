7ed185e55721b23428c4ad785385982789ace0b2
ErrorInever <errorinever@gmail.com>
2019-10-01 16:33:13 +0300

    add more parameters

--- model/cfg.py
+++ model/cfg.py
@@ -5,8 +5,30 @@ __C = edict()
 # Consumers can get config by:
 cfg = __C
 
-# Anchor scales
-__C.ANCHOR_SCALES = [8, 16, 32]
+# RPN
+__C.ANCHOR = edict()
+__C.ANCHOR.SCALES = [8, 16, 32]
+__C.ANCHOR.RATIOS = [0.5, 1, 2]
 
-# Anchor ratios
-__C.ANCHOR_RATIOS = [0.5, 1, 2]
+# Normalization
+__C.TRANSFORM = edict()
+__C.TRANSFORM.MEAN = [0.485, 0.456, 0.406]
+__C.TRANSFORM.STD = [0.229, 0.224, 0.225]
+
+# Train
+__C.TRAIN = edict()
+__C.TRAIN.EPOCHS = 100
+__C.TRAIN.LEARNING_RATE = 1.0e-3
+__C.TRAIN.BATCH_SIZE = 128
+
+# scheduler
+__C.TRAIN.SCHEDULER = edict()
+__C.TRAIN.SCHEDULER.STEP_SIZE = 5
+__C.TRAIN.SCHEDULER.GAMMA = 0.1
+
+# Stochastic gradient descent with momentum
+__C.TRAIN.SGD = edict()
+__C.TRAIN.SGD.MOMENTUM = 0.9
+
+# L2 regularization
+__C.WEIGHT_DECAY = 0.0005
